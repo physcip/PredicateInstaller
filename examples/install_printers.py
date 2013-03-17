7e9a03a5dafa18223169a762b152220304aba94d
Michael Kuron <m.kuron@gmx.de>
2013-03-17 11:06:36 +0100

    Enhanced printer example: now deletes pre-existing printer and allows specifying PPD options

--- examples/install_printers.py
+++ examples/install_printers.py
@@ -9,10 +9,20 @@ printers.append({
 	'model' : "MANUFACTURER:HP;MODEL:Color LaserJet CP4020-CP4520",
 	'uri' : 'ipp://purple.physcip.uni-stuttgart.de:631/printers/wario.physcip.uni-stuttgart.de',
 	'ppd' : '/Library/Printers/PPDs/Contents/Resources/HP Color LaserJet CP4020 CP4520 Series.gz',
-	'name' : 'wario'
+	'name' : 'wario',
+	'options' : {
+		'InstalledMemory' : '1048576',
+		'OptionalTray' : 'HP3x500PaperFeeder',
+		'HPOption_Duplexer' : 'True',
+		'HPOption_Disk' : 'True',
+		'PageSize' : 'A4',
+		'Duplex' : 'DuplexNoTumble',
+		'HPBookletPageSize' : 'A4',
+	},
 })
 
 # To figure out the model, run /System/Library/SystemConfiguration/PrinterNotifications.bundle/Contents/MacOS/makequeues -q, which performs a Bonjour scan. 
+# To figure out available options, run lpoptions -d printername -l after you initially added the printer
 
 for printer in printers:
 	try:
@@ -24,8 +34,24 @@ for printer in printers:
 		if not os.path.exists(printer['ppd']):
 			raise Exception('PPD not found')
 		
+		# delete print queue if it already exists
+		DEVNULL = open(os.devnull, 'w')
+		if subprocess.call(['/usr/bin/lpstat', '-p', printer['name']], stdout=DEVNULL, stderr=DEVNULL) == 0: # printer exists
+			subprocess.call(['/usr/sbin/lpadmin', '-x', printer['name']])
+		DEVNULL.close()
+		
 		# add print queue
 		subprocess.check_call(['/System/Library/SystemConfiguration/PrinterNotifications.bundle/Contents/MacOS/makequeues', '-v', printer['uri'], '-d', printer['ppd'], '-q', printer['name']])
 		
+		# set printer options
+		if 'options' in printer and len(printer['options']) > 0:
+			optionstrings = []
+			for k,v in printer['options'].iteritems():
+				optionstrings.append('-o')
+				optionstrings.append('%s=%s' % (k,v))
+		
+			subprocess.check_call(['/usr/sbin/lpadmin', '-p', printer['name']] + optionstrings)
+		
 	except Exception as e:
 		print "Could not add printer %s: %s" % (printer['name'], e)
+
