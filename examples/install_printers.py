6013823fef4c41541173827440ab31129a8d5db1
Michael Kuron <michael@physcip.uni-stuttgart.de>
2013-10-12 15:03:12 +0200

    Use lpadmin instead of makequeues and make sure CUPS is really running before we start.

--- examples/install_printers.py
+++ examples/install_printers.py
@@ -7,9 +7,10 @@ printers = []
 printers.append({
 	'predicate' : '("printing software" IN tags OR "printer update" IN tags) AND "MANUFACTURER:HP;MODEL:Color LaserJet CP4020-CP4520" IN tags',
 	'model' : "MANUFACTURER:HP;MODEL:Color LaserJet CP4020-CP4520",
-	'uri' : 'ipp://purple.physcip.uni-stuttgart.de:631/printers/ghost_physcip_uni_stuttgart_de',
+	'uri' : 'ipp://robert.physcip.uni-stuttgart.de:631/printers/ghost_physcip_uni_stuttgart_de',
 	'ppd' : '/Library/Printers/PPDs/Contents/Resources/HP Color LaserJet CP4020 CP4520 Series.gz',
 	'name' : 'ghost',
+	'location': 'CIP Pool Physik',
 	'options' : {
 		'InstalledMemory' : '1048576',
 		'OptionalTray' : 'HP3x500PaperFeeder',
@@ -22,15 +23,17 @@ printers.append({
 	'remote' : True,
 })
 
-# To figure out the model, run /System/Library/SystemConfiguration/PrinterNotifications.bundle/Contents/MacOS/makequeues -q, which performs a Bonjour scan. 
+# To figure out the model, run /System/Library/SystemConfiguration/PrinterNotifications.bundle/Contents/MacOS/makequeues with the -q flag (on OS X 10.6) or -B flag (on OS X 10.8), which performs a Bonjour scan. 
 # To figure out available options, run lpoptions -d printername -l after you initially added the printer
 
 predicate_installer = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'predicate_installer.py') # in this directory
 if not os.path.exists(predicate_installer):
 	predicate_installer = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'predicate_installer.py') # in parent directory
 
-# make sure CUPS is running
+# restart CUPS and make sure it's running
+subprocess.call(['/bin/launchctl', 'unload', '/System/Library/LaunchDaemons/org.cups.cupsd.plist'])
 subprocess.call(['/bin/launchctl', 'load', '/System/Library/LaunchDaemons/org.cups.cupsd.plist'])
+subprocess.check_call(['/bin/launchctl', 'start', 'org.cups.cupsd'])
 
 for printer in printers:
 	try:
@@ -49,7 +52,7 @@ for printer in printers:
 		DEVNULL.close()
 		
 		# add print queue
-		subprocess.check_call(['/System/Library/SystemConfiguration/PrinterNotifications.bundle/Contents/MacOS/makequeues', '-v', printer['uri'], '-d', printer['ppd'], '-q', printer['name']])
+		subprocess.check_call(['/usr/sbin/lpadmin', '-p', printer['name'], '-v', printer['uri'], '-m', printer['ppd'], '-L', printer['location'], '-E', '-o', 'printer-is-shared=false'])
 		
 		# set printer options
 		if 'options' in printer and len(printer['options']) > 0:
