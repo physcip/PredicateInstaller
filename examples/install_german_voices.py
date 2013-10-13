099ff76e6b7ea10535fd85de1709a53baa9c9252
Michael Kuron <michael@physcip.uni-stuttgart.de>
2013-10-13 11:55:46 +0200

    Make voices example work on 10.7-10.9

--- examples/install_german_voices.py
+++ examples/install_german_voices.py
@@ -2,11 +2,20 @@
 
 import subprocess
 import os
+import platform
+
+num = int(platform.release().split('.')[0])-10 # 13=>3: Mavericks, 12=>2: Mountain Lion, 11=>1: Lion
+if num <= 0:
+	raise Exception("Voices are not available in OS X below 10.7")
+if num == 1:
+	num = ''
+else:
+	num = '_%d' % num
 
 pkgs = [
-	'"VOICEID:com.apple.speech.synthesis.voice.anna.premium_2" IN tags',
-	'"VOICEID:com.apple.speech.synthesis.voice.steffi.premium_2" IN tags',
-	'"VOICEID:com.apple.speech.synthesis.voice.yannick.premium_2" IN tags'
+	'"VOICEID:com.apple.speech.synthesis.voice.anna.premium%s" IN tags' % num,
+	'"VOICEID:com.apple.speech.synthesis.voice.steffi.premium%s" IN tags' % num,
+	'"VOICEID:com.apple.speech.synthesis.voice.yannick.premium%s" IN tags' % num
 ]
 
 for pkg in pkgs:
