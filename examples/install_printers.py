1c52bb971737035da76adc23a5b527237496cdca
Michael Kuron <michael@physcip.uni-stuttgart.de>
2014-03-27 15:41:28 +0100

    Printer example now downloads PPDs from CUPS server

--- examples/install_printers.py
+++ examples/install_printers.py
@@ -2,68 +2,7 @@
 
 import subprocess
 import os
-import platform
-
-darwin = int(platform.release().split('.')[0])
-
-printers = []
-printers.append({
-	'predicate' : '("printing software" IN tags OR "printer update" IN tags) AND "MANUFACTURER:HP;MODEL:Color LaserJet CP4020-CP4520" IN tags',
-	'model' : "MANUFACTURER:HP;MODEL:Color LaserJet CP4020-CP4520",
-	'uri' : 'ipp://robert.physcip.uni-stuttgart.de:631/printers/ghost_physcip_uni_stuttgart_de',
-	'ppd' : '/Library/Printers/PPDs/Contents/Resources/HP Color LaserJet CP4020 CP4520 Series.gz',
-	'name' : 'ghost',
-	'location': 'CIP Pool Physik',
-	'options' : {
-		'InstalledMemory' : '1048576',
-		'OptionalTray' : 'HP3x500PaperFeeder',
-		'HPOption_Duplexer' : 'True',
-		'HPOption_Disk' : 'True',
-		'PageSize' : 'A4',
-		'Duplex' : 'DuplexNoTumble',
-		'HPBookletPageSize' : 'A4',
-	},
-	'remote' : True,
-})
-printers.append({
-	'uri' : 'ipp://robert.physcip.uni-stuttgart.de:631/printers/nymeria_physcip_uni_stuttgart_de',
-	'ppd' : '/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTAC454e.gz',
-	'name' : 'nymeria',
-	'location': 'CIP Pool Physik',
-	'options' : {
-		'PaperSources' : 'PC410',
-		'Finisher' : 'FS533',
-		'KMPunchUnit' : 'PK519-EU4',
-		'SelectColor' : 'Grayscale',
-#		'ColorModel' : 'Gray',
-#		'SimulationProfile' : 'None',
-		'KMDuplex' : 'Double',
-	},
-#	'editppd' : {
-#		'^\*DefaultKMDuplex: .*$' : '*DefaultKMDuplex: Double',
-#		'^\*DefaultSelectColor: .*$' : '*DefaultSelectColor: Grayscale',
-#	},
-	'remote' : True,
-})
-if darwin == 10:
-	printers[-1]['download'] = 'https://o.cses.konicaminolta.com/file/Default.aspx?FilePath=DL/201307/25042126/BHC554ePSMacOS106_302MU.dmg'
-	printers[-1]['version'] = '3.0.2'
-	printers[-1]['pkgpath'] = 'A4/bizhub_C554_106.pkg'
-elif darwin == 11:
-	printers[-1]['download'] = 'https://o.cses.konicaminolta.com/file/Default.aspx?FilePath=DL/201307/25042758/BHC554ePSMacOS107_302MU.dmg'
-	printers[-1]['version'] = '3.0.2'
-	printers[-1]['pkgpath'] = 'A4/bizhub_C554_107.pkg'
-elif darwin == 12:
-	printers[-1]['download'] = 'https://o.cses.konicaminolta.com/file/Default.aspx?FilePath=DL/201307/25043316/BHC554ePSMacOS108_302MU.dmg'
-	printers[-1]['version'] = '3.0.2'
-	printers[-1]['pkgpath'] = 'A4/bizhub_C554_108.pkg'
-elif darwin == 13:
-	printers[-1]['download'] = 'https://o.cses.konicaminolta.com/file/Default.aspx?FilePath=dl/201401/13110019/bizhub_C554e_109.dmg'
-	printers[-1]['version'] = '4.5.3'
-	printers[-1]['pkgpath'] = 'bizhub_C554_C364_109.pkg'
-
-# To figure out the model, run /System/Library/SystemConfiguration/PrinterNotifications.bundle/Contents/MacOS/makequeues with the -q flag (on OS X 10.6) or -B flag (on OS X 10.8), which performs a Bonjour scan. 
-# To figure out available options, run lpoptions -d printername -l after you initially added the printer
+from install_printers_config import printers
 
 predicate_installer = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'predicate_installer.py') # in this directory
 if not os.path.exists(predicate_installer):
@@ -76,12 +15,12 @@ subprocess.check_call(['/bin/launchctl', 'start', 'org.cups.cupsd'])
 
 for printer in printers:
 	try:
-		if not os.path.exists(printer['ppd']) and 'predicate' in printer:
+		if 'ppd' in printer and not os.path.exists(printer['ppd']) and 'predicate' in printer:
 			# mark printer as connected
 			subprocess.check_call(['/usr/libexec/PlistBuddy', '-c', "Add InstalledPrinters: string " + printer['model'], '/Library/Printers/InstalledPrinters.plist'])
 			# install printer driver
 			subprocess.call(['/usr/bin/python', predicate_installer, printer['predicate']])
-		elif 'download' in printer:
+		elif 'ppd' in printer and 'download' in printer:
 			from distutils.version import StrictVersion
 			try:
 				if not os.path.exists(printer['ppd']):
@@ -106,8 +45,14 @@ for printer in printers:
 				print "Installing %s" % pkgpath
 				subprocess.check_call(['/usr/sbin/installer', '-package', pkgpath, '-target', '/'])
 				subprocess.check_call(['/usr/bin/hdiutil', 'detach', mountpoint])
+		elif 'ppd' not in printer and 'download' in printer and printer['download'].endswith('.ppd'):
+			printer['ppd'] = os.path.join('/usr/share/cups/model', os.path.basename(printer['download']))
+			print "Downloading %s" % os.path.basename(printer['ppd'])
+			subprocess.check_call(['/usr/bin/curl', '-L', '-o', printer['ppd'], printer['download']])
 		if not os.path.exists(printer['ppd']):
 			raise Exception('PPD not found')
+		if printer['ppd'].startswith('/usr/share/cups/model'):
+			printer['ppd'] = os.path.basename(printer['ppd']) # relative path required
 		
 		# delete print queue if it already exists
 		DEVNULL = open(os.devnull, 'w')
